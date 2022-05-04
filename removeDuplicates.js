08f08caeb3785cb71d0e6886d62e35f5be857c38
nchathu2014 <nchathu2014@gmail.com>
2022-05-04 19:36:07 +0530

    Update removeDuplicates.js

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,5 +1,12 @@
 const array = [1,1,1,2,4,5,1,5,6];
 
+//TODO: Can you follow this approach, which is O(N)
+/*
+    const lookup = {};
+    for(let item of array) if(!lookup[item]) lookup[item] = 1;
+    return Object.keys(lookup);
+*/
+
 Array.prototype.removeDuplicates = function() {
     let newArray = [];
     for (let i = 0; i < this.length; i++) {
